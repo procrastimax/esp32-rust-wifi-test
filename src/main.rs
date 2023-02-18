64b385949796a9d409002b59d2a8350a419527cd
procrastimax <mmeheykeroth@protonmail.com>
2023-02-18 15:59:03 +0100

    Adds code for wifi setup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,56 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
+use esp_idf_hal::peripherals::Peripherals;
+use std::{thread::sleep, time::Duration};
+
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::EspWifi};
+
+use embedded_svc::wifi::{ClientConfiguration, Configuration, Wifi};
+
+#[toml_cfg::toml_config]
+pub struct Credentials {
+    #[default("")]
+    ssid: &'static str,
+    #[default("")]
+    passphrase: &'static str,
+}
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
+    println!(
+        "Starting to connect to wifi: {:?} with password: {:?}",
+        CREDENTIALS.ssid, CREDENTIALS.passphrase
+    );
 
-    println!("Hello, world!");
+    let peripherals = Peripherals::take().unwrap();
+    let sys_loop = EspSystemEventLoop::take().unwrap();
+    let nvs = EspDefaultNvsPartition::take().unwrap();
+
+    let mut wifi_driver = EspWifi::new(peripherals.modem, sys_loop, Some(nvs)).unwrap();
+
+    wifi_driver
+        .set_configuration(&Configuration::Client(ClientConfiguration {
+            ssid: CREDENTIALS.ssid.into(),
+            password: CREDENTIALS.passphrase.into(),
+            ..Default::default()
+        }))
+        .unwrap();
+
+    wifi_driver.start().unwrap();
+    wifi_driver.connect().unwrap();
+    while !wifi_driver.is_connected().unwrap() {
+        let config = wifi_driver.get_configuration().unwrap();
+        println!("Waiting for station {:?}", config)
+    }
+    println!("Connection established");
+
+    loop {
+        println!(
+            "IP info: {:?}",
+            wifi_driver.sta_netif().get_ip_info().unwrap()
+        );
+        sleep(Duration::new(10, 0));
+    }
 }
