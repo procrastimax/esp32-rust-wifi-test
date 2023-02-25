74170bcf1d89c05a5b3d02b3a4a18b42e9816f8a
procrastimax <mmeheykeroth@protonmail.com>
2023-02-25 18:58:12 +0100

    Modifies partition table, adds HTTP Post function
    The modified partition table is needed, to increase the usable flash
    size to 3MB.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wi
 use embedded_svc::wifi::{ClientConfiguration, Configuration, Wifi};
 
 use embedded_svc::{
-    http::{client::Client as HttpClient,Status},
+    http::{client::Client as HttpClient, Method, Status},
+    io::Write,
     utils::io,
 };
 
@@ -58,7 +59,6 @@ fn main() {
     // create http client
     let mut client = HttpClient::wrap(
         EspHttpConnection::new(&HttpConfiguration {
-            crt_bundle_attach: Some(esp_idf_sys::esp_crt_bundle_attach),
             ..Default::default()
         })
         .unwrap(),
@@ -67,6 +67,8 @@ fn main() {
     thread::sleep(time::Duration::from_secs(5));
 
     get(&mut client, "http://ifconfig.net");
+
+    post_request(&mut client);
 }
 
 fn get(client: &mut HttpClient<EspHttpConnection>, url: &str) {
@@ -89,4 +91,49 @@ fn get(client: &mut HttpClient<EspHttpConnection>, url: &str) {
     };
 
     while body.read(&mut buf).unwrap() > 0 {}
+}
+
+/// Send a HTTP POST request.
+fn post_request(client: &mut HttpClient<EspHttpConnection>) -> anyhow::Result<()> {
+    // Prepare payload
+    let payload = b"Hello world!";
+
+    // Prepare headers and URL
+    let content_length_header = format!("{}", payload.len());
+    let headers = [
+        ("accept", "text/plain"),
+        ("content-type", "text/plain"),
+        ("connection", "close"),
+        ("content-length", &*content_length_header),
+    ];
+    let url = "http://example.org/";
+
+    // Send request
+    let mut request = client.post(&url, &headers)?;
+    request.write_all(payload)?;
+    request.flush()?;
+    println!("-> POST {}", url);
+    let mut response = request.submit()?;
+
+    // Process response
+    let status = response.status();
+    println!("<- {}", status);
+    println!();
+    let (_headers, mut body) = response.split();
+    let mut buf = [0u8; 1024];
+    let bytes_read = io::try_read_full(&mut body, &mut buf).map_err(|e| e.0)?;
+    println!("Read {} bytes", bytes_read);
+    match std::str::from_utf8(&buf[0..bytes_read]) {
+        Ok(body_string) => println!(
+            "Response body (truncated to {} bytes): {:?}",
+            buf.len(),
+            body_string
+        ),
+        Err(e) => eprintln!("Error decoding response body: {}", e),
+    };
+
+    // Drain the remaining response bytes
+    while body.read(&mut buf)? > 0 {}
+
+    Ok(())
 }
